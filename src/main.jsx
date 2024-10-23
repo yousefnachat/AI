d8c0ec41f513340fe3b111ff265465ef5612ad08
youssef <youssefnh05@gmail.com>
2024-10-22 19:03:38 -0700

    Adding hero and AI definition section

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Nav from './components/Navbar'
 import Hero from './components/Hero'
+import AiSection from './components/AiSection'
 import './index.css'
 
 
@@ -9,5 +10,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Nav />
     <Hero />
+    <AiSection />
   </StrictMode>
 )
